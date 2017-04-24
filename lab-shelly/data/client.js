9dbfb28b9be6fdf6627016d01ce22e0fe53f507e
Shelly Tang <shellytang@gmail.com>
2017-04-24 16:55:29 -0700

    created TCP server using net module

--- lab-shelly/data/client.js
+++ lab-shelly/data/client.js
@@ -6,4 +6,4 @@ module.exports = function(socket) {
   this.socket = socket;
   this.nickName = uuid.v4();
   this.userName = `${Math.random()}`;
-}
+};
---
+++ lab-shelly/server.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const Client = require('.model/client');
+const net = require('net');
+const EE = require('events').EventEmitter;
+const ee = new EE();
+const server = net.createServer();
+const PORT = process.env.PORT || 3000;
+const pool = [];
