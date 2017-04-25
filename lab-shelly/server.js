e5b07982662826b3a8ea727de2d04839995c0de6
Shelly Tang <shellytang@gmail.com>
2017-04-24 17:05:51 -0700

    setup inital ee

--- lab-shelly/server.js
+++ lab-shelly/server.js
@@ -7,3 +7,23 @@ const ee = new EE();
 const server = net.createServer();
 const PORT = process.env.PORT || 3000;
 const pool = [];
+
+server.on('connection', socket => {
+  let client = new Client(socket);
+  pool.push(client);
+  pool.forEach(c => c.socket.write(`${client.userName} has connected!\n`));
+
+  socket.on('data', data => {
+    let command = data.toString().split('').shift().trim();
+    if(command.startsWith('/all')) {
+      ee.emit(command, client, data.toString().split(' ').slice(1).join(' '));
+      return;
+    }
+
+    ee.emit('default', client, data.toString());
+
+  });
+
+});
+
+server.listen(PORT, () => console.log(`Listening on: ${PORT}`));
